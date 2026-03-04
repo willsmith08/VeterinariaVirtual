d1c6a08008346732f2f9797e45d9446b55f057f3
Will Smith <willtoncel@gmail.com>
2026-03-04 18:44:32 -0500

    Se implementó la view del header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/header.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dog & cat</title>
+    <link rel="icon" href="/imgs/LOGO.png">
+</head>
+<body>
+    <div class="todo_fijado">
+        <div class="arriba-header">
+            <img src="/imgs/logo_phone.png" alt="" class="PHONE">
+            <b>: 3144542057</b>
+        </div>
+        <header>
+            <a href="index.html"><img src="/imgs/LOGO.png" alt="¡ERROR!" class="logo_pagina"></a>
+            <p><b>Contactanos: 3144542057</b></p>
+            <div class="phone-contac">
+                <img src="/imgs/logo_phone.png" alt="">
+                <b>: 3144542057</b>
+            </div>
+            <div class="redes_sociales">
+                <div class="socia"><img src="/imgs/Logo_x.png" alt="¡ERROR!" class="social"></div>
+                <div class="socia"><img src="/imgs/facebook_logo.png" alt="¡ERROR!" class="social"></div>
+                <div class="socia"><img src="/imgs/youtube_logo.png" alt="¡ERROR!" class="social"></div>
+                <div class="socia"><img src="/imgs/instagram_logo.png" alt="¡ERROR!" class="social"></div>
+            </div>
+            <input type="checkbox" id="check">
+            <label for="check" class="abrir-menu">
+                &#8801
+            </label>
+            <nav class="menu">
+                <a href="productos.html"><b>PRODUCTOS</b></a>
+                <a href="servicios.html"><b>SERVICIOS</b></a>
+                <a href="promociones.html"><b>PROMOCIONES</b></a>
+                <a href="nosotros.html"><b>NOSOTROS</b></a>
+                <a href="registro_citas.html"><b>CITAS</b></a>
+                <label for="check" class="cerrar-menu">
+                    &#215
+                </label>
+            </nav>
+            <nav class="opcs_pantallas">
+                <a href="productos.html" class="opc_pantalla"><b>PRODUCTOS</b></a>
+                <a href="servicios.html" class="opc_pantalla"><b>SERVICIOS</b></a>
+                <a href="promociones.html" class="opc_pantalla"><b>PROMOCIONES</b></a>
+                <a href="nosotros.html" class="opc_pantalla"><b>NOSOTROS</b></a>
+                <a href="registro_citas.html" class="opc_pantalla"><b>CITAS</b></a>
+                <nav class="opc_pantalla1">
+                    <input type="checkbox" id="checked-user">
+                    <label for="checked-user" class="opc_pantall2">
+                        <img class="opc_pantalla2" src="/imgs/User logo.png" alt="¡ERROR!">
+                    </label>
+                    <div class="ini-regis">
+                        <a href="iniciar_sesion.html">Iniciar sesion</a>
+                        <p><b>¿Aun no tienes cuenta?</b></p>
+                        <a href="registrarse.html" class="regi">Registrarse</a>
+                    </div>
+                    <input type="checkbox" id="checked-noti">
+                    <label for="checked-noti" class="opc_pantall2">
+                        <img class="opc_pantalla2" src="/imgs/logo notificaciones.png" alt="¡ERROR!">
+                    </label>
+                    <div class="notificaciones">
+                        <header>
+                            <B>NOTIFICACIONES</B>
+                        </header>
+                        <img src="/imgs/no_hay-noti.png" alt="">
+                    </div>
+                </nav>
+            </nav>
+        </header>
+        <div class="container_debajo_header">
+            <a href="carrito_compra.html"><img src="/imgs/carrito.png" alt="" class="carrito-deb"></a>
+            <nav>
+                <input type="checkbox" id="user2">
+                <label for="user2" class="user2">
+                    <img src="/imgs/User logo.png" alt="¡ERROR!">
+                </label>
+                <div class="ini-regi">
+                    <a href="iniciar_sesion.html" class="iniciar_sesion1">Iniciar sesion</a>
+                    <a href="iniciar_sesion-telefono.html" class="iniciar_sesion2">Iniciar sesion</a>
+                    <p><b>¿Aun no tienes cuenta?</b></p>
+                    <a href="registrarse.html" class="regi">Registrarse</a>
+                </div>
+                <input type="checkbox" id="noti2">
+                <label for="noti2" class="noti2">
+                    <img src="/imgs/logo notificaciones.png" alt="¡ERROR!">
+                </label>
+                <div class="notificacion">
+                    <header>
+                        <B>NOTIFICACIONES</B>
+                    </header>
+                    <img src="/imgs/no_hay-noti.png" alt="">
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    @yield('content')
+
+</body>
+</html>
